8ad364454ab7c917fd50eebe542ca234d015a456
John Carlee <jcarlee@gmail.com>
2018-12-17 12:57:16 -0600

    needs work

--- rail-sql.py
+++ rail-sql.py
@@ -1,9 +1,11 @@
 from tkinter import filedialog
 from tkinter import *
+from tkinter import ttk
+import csv
 
 master = Tk()
 master.title("Rail Car Dimensions")
-master.geometry("300x267")
+master.geometry("400x635")
 
 field_names = ['Id', 'Name', 'Description', 'Cabin Length', 'Wheel Length', 'Wheel Anchor', 'Track Guage',
                'Envelope', 'User ID', 'URL Id', 'More Info']
@@ -29,20 +31,42 @@ def make_sql_report():
     wheel_length = try_input(float(e5.get()))
     wheel_anchor = try_input(float(e6.get()))
     track_guage = try_input(float(e7.get()))
-    envelope = try_input(str(e8.get()))
-    user_id = try_input(int(e9.get()))
-    url_id = try_input(int(e10.get()))
-    more_info = try_input(str(e11.get()))
-    input_values = [objectid, name, desc, cabin_length, wheel_length, wheel_anchor, track_guage, envelope, user_id,
-                    url_id, more_info]
+    user_id = try_input(int(e10.get()))
+    url_id = try_input(int(e11.get()))
+    more_info = try_input(str(e12.get()))
+    # input_values = [objectid, name, desc, cabin_length, wheel_length, wheel_anchor, track_guage, envelope, user_id,
+    #                url_id, more_info]
     sql_file = open(master.filename, 'w')
     sql_file.write("Insert into [RailCar]([Id], [Name], [Description], [CabinLength], [WheelLength], [WheelAnchor], "
                    "[TrackGuage], [Envelope], [UserId], [UrlId], [MoreInfo])\n\t")
     sql_file.write(
         "Values({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10});\n".format(
-            objectid, name, desc, cabin_length, wheel_length, wheel_anchor, track_guage, envelope, user_id, url_id,
+            objectid, name, desc, cabin_length, wheel_length, wheel_anchor, track_guage, None, user_id, url_id,
             more_info))
     sql_file.close()
+
+# m to ussft m * 1200/3937
+# ft to ussft 1 = 0.999998000004
+#
+
+
+def transpose():
+    env = e8.get("1.0", 'end-1c')
+    lol = csv.reader(env, delimiter='\t')
+    print(lol)
+#    for line in lol:
+#       print(line)
+
+
+def envelope():
+    env = e8.get("1.0", 'end-1c')
+    if e9.get() == 'm':
+        pass
+    elif e9.get() == 'ft':
+        pass
+    elif e9.get() == 'ussft':
+        pass
+    print(env)
 
 
 for i in field_names:
@@ -71,25 +95,31 @@ e6.insert(END, '0.00')
 e7 = Entry(master)  # TrackGuage
 e7.insert(END, '0.00')
 
-e8 = Entry(master)  # Envelope
+e8 = Text(master)  # Envelope
 e8.insert(END, '**Copy and Paste**')
 
-e9 = Entry(master)  # UserID
-e9.insert(END, '1')
+uom = ['m', 'ft', 'ussft']
 
-e10 = Entry(master)  # URL Id
-e10.insert(END, '12')
+e9 = ttk.Combobox(master, values=uom)
+e9.set('m')
 
-e11 = Entry(master)  # MoreInfo
-e11.insert(END, 'google.com')
+e10 = Entry(master)  # UserID
+e10.insert(END, '1')
 
-e_list = [e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11]
+e11 = Entry(master)  # URL Id
+e11.insert(END, '12')
+
+e12 = Entry(master)  # MoreInfo
+e12.insert(END, 'google.com')
+
+e_list = [e1, e2, e3, e4, e5, e6, e7, e8, e10, e11, e12]
 
 for i in e_list:
-    i.grid(row=e_list.index(i), column=1, sticky=W + E)
+    i.grid(row=e_list.index(i), column=1, sticky=W + E, padx=10)
 
 # Create Save button
-Button(master, text='Save', command=make_sql_report).grid(row=12, column=1, sticky=W+E, pady=4, padx=10)
+Button(master, text='Save', command=make_sql_report).grid(row=13, column=1, sticky=W+E, pady=4, padx=(0, 10))
+Button(master, text='Envelope', command=transpose).grid(row=13, column=0, sticky=W+E, pady=4, padx=10)
 
 # Ensure text boxes expand with window
 master.columnconfigure(1, weight=1)
